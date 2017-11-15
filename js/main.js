714338dcd8662a7a98d95456d3f71f47d9ea9f36
Arel <arellin0108@gmail.com>
2017-11-15 15:14:15 +0800

    Create JQuery version to do list

--- js/main.js
+++ js/main.js
@@ -1,152 +1,139 @@
-(function main() {
-	const todolist = document.getElementsByClassName('todolist')[0];
-	const listItem = document.getElementsByClassName('list-item');
-	var filterMode = 'all';
-	var listData = [];
+(function() {
 
-	// attach createList function to button
-	var addListButton = document.getElementsByClassName('add-btn');
-	addListButton[0].onclick = function() {
+	var filterType = 'all';
+	var itemData = [
+		{
+			done: true,
+			content: 'This is for test',
+		}
+	];
+
+	renderList();
+
+	$('.add-confirm').on('click', () => {
 		createList();
-	};
+	});
 
-	//attach filterToggle to button
-	var filterDone = document.getElementById('done');
-	filterDone.onclick = function() {
-		filterToggle('done');
+	$('.all').on('click', () => {
+		filterType = 'all';
+		renderList();
+	});
+
+	$('.done').on('click', () => {
+		filterType = true;
+		renderList();
+	});
+
+	$('.undone').on('click', () => {
+		filterType = false;
+		renderList();
+	});		
+
+	// render item data array with tags
+	function renderList() {
+
+		// delete all child
+		$('.todolist').empty();
+
+		// filter before render
+		var filterItemData = filterList(filterType);
+
+
+
+		// render all items out
+		for(var i = 0; i < filterItemData.length; i++) {
+			$('<div/>')
+				.addClass(() => {
+					if(filterItemData[i].done) {
+						return 'list-item complete';
+					} else {
+						return 'list-item';
+					}
+				})
+				.append(
+					$('<span/>')
+						.addClass('list-done')
+						.text(function() {
+							if(filterItemData[i].done) {
+								return 'Done';
+							} else {
+								return 'Ongoing';
+							}
+						})
+						.on('click', e => {
+							completeList($(e.currentTarget).closest('.list-item'));
+						}),
+					$('<span/>')
+						.addClass('list-content')
+						.text(filterItemData[i].content)
+						.on('click', filterItemData[i], e => {
+							editList(e);
+						}),
+					$('<a/>')
+						.attr('href','#')
+						.addClass('list-delete')
+						.text('Delete')
+						.on('click', e => {
+							e.stopPropagation();
+							deleteList(e.currentTarget.closest('.list-item'));
+						})
+				)
+				.appendTo('.todolist');			
+		}
 	}
 
-	var filterUndone = document.getElementById('undone');
-	filterUndone.onclick = function() {
-		filterToggle('undone');
-	}
-
-	var filterAll = document.getElementById('all');
-	filterAll.onclick = function() {
-		filterToggle('all');
-	}
-
-	// Create a new list
+	// add a new list data into item data array
 	function createList() {
-		var title = document.getElementsByClassName('add-list-title')[0].value;
-		var date = document.getElementsByClassName('add-list-date')[0].value;
-		var detail = document.getElementsByClassName('add-list-detail')[0].value;
-		listData.push({
-			title: title,
-			date: date,
-			detail: detail,
-			complete: false,
-			display: 'none',
+		itemData.push({
+			done: false,
+			content: $('.add-content').val(),
 		});
 		renderList();
 	}
 
-	// Toggle between complete and incomplete
-	function completeList(self) {
-		console.log(self);
-		var currentIndex = Array.from(self.parentNode.children).indexOf(self);
-		if(listData[currentIndex].complete) {
-			listData[currentIndex].complete = false;
-			listData[currentIndex].display = 'none';
+	// delete a specific list data and its render 
+	function deleteList(target) {
+		// remove data, should before remove rendering
+		itemData.splice($(target).index(), 1);
+		// remove rendering
+		target.remove();		
+	}
+
+	// toggle an existing list between complete and incomplete
+	function completeList(target) {
+		if(!itemData[target.index()].done) {
+			itemData[target.index()].done = true;
 		} else {
-			listData[currentIndex].complete = true;
-			listData[currentIndex].display = 'block';
+			itemData[target.index()].done = false;
 		}
 		renderList();
 	}
 
-	// Delete specified completed list
-	function deleteList(self) {
-		var currentIndex = Array.from(self.parentNode.children).indexOf(self);
-		console.log(currentIndex);
-		console.log(listData.splice(currentIndex, 1));
-		renderList();
+	// edit an existing list
+	function editList(target) {
+		$(target.currentTarget).replaceWith(
+			$('<input/>')
+				.val(target.data.content)
+				.on('keypress', e => {
+					if(e.which == 13) {
+						// edit data
+						itemData[$(e.currentTarget).closest('.list-item').index()].content = $(e.currentTarget).val();
+						renderList();
+					}
+				})
+		);		
 	}
 
-	// Filter the finished list and render
-	function filterToggle(value) {
-		filterMode = value;
-		renderList();
-	}
-
-	// Render all list
-	function renderList() {
-		var newListItem;
-		var newListTitle;
-		var newListDate;
-		var newListDetail;
-		var delButton;
-		var filterListData;
-
-		//clean before every rendering
-		deleteAll();
-
-		// Filter listData before rendering
-		if(filterMode === 'undone') {
-			filterListData = listData.filter(function(listData) {
-				return listData.complete === false;
+	// filter list before render
+	function filterList(type) {
+		var filteredData;
+		if(typeof type === 'boolean') {
+			filteredData = itemData.filter(data => {
+				return data.done === type;
 			});
-		} else if(filterMode === 'done') {
-			filterListData = listData.filter(function(listData) {
-				return listData.complete === true;
-			});
+			return filteredData;	
 		} else {
-			filterListData = listData;
-		}
-
-		// Start rendering
-		for(var i = 0; i < filterListData.length; i++) {
-			newListItem = document.createElement('DIV');
-			if(filterListData[i].complete) {
-				newListItem.className = 'list-item complete';
-			} else {
-				newListItem.className = 'list-item';				
-			}
-
-			newListTitle = document.createElement('H3');
-			newListTitle.className = 'list-title';
-			newListTitle.appendChild(document.createTextNode(filterListData[i].title));
-
-			newListDate = document.createElement('P');
-			newListDate.className = 'list-date';
-			newListDate.appendChild(document.createTextNode(filterListData[i].date));
-
-			newListDetail = document.createElement('P');
-			newListDetail.className = 'list-detail';
-			newListDetail.appendChild(document.createTextNode(filterListData[i].detail));
-
-			delButton = document.createElement('A');
-			delButton.className = 'delete-button';
-			delButton.appendChild(document.createTextNode('Delete'));
-			delButton.setAttribute('href', '#');
-			delButton.style.display = filterListData[i].display;
-
-			newListItem.appendChild(newListTitle);
-			newListItem.appendChild(newListDate);
-			newListItem.appendChild(newListDetail);
-			newListItem.appendChild(delButton);
-
-			// give functions to each list items
-			newListItem.onclick = function() {
-				var self = this;
-				completeList(self);
-			};
-
-			todolist.appendChild(newListItem);
-
-			// delete a selected list item
-			delButton.onclick = function() {
-				var self = this.parentNode;
-				deleteList(self);
-				event.stopPropagation();
-			}
-		}
-	}
-
-	// Clean all list
-	function deleteAll() {
-		while(todolist.firstChild) {
-			todolist.removeChild(todolist.firstChild);
+			return itemData;
 		}
 	}
 
